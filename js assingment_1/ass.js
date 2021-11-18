d8f936be9cdc48bf3cf5db52eb13f0303612cce4
sushanttwayana <65543151+sushanttwayana@users.noreply.github.com>
2021-11-18 22:57:46 +0545

    Add files via upload

---
+++ js assingment_1/ass.js
@@ -0,0 +1,21 @@
+// var myName="Sushant Twayana";
+// console.log(myName);
+// prompt(myName);
+
+//  let classStudents = " Ram , Hari , Shyam"; // camel convention
+// console.log(classStudents);
+
+// let my_school_name ='ABC Secondary School';
+// let my_age = 20; // snake naming convention
+// console.log (my_age + 1);
+
+// let string1 = prompt(" Enter the string you want ");
+// let string2 = prompt(' Enter another string');
+// console.log( string1 +' ' + string2);
+
+console.error(" OPPS!!! Something went wrong ");
+console.warn(' Please check your code again....');
+
+//  In JS all the variables are dyanmic (both in type and existence), and even the code is dynamic ie variables and 
+// functions can be declared at the runtime as well as function and can be changed accordingly. When used in a browser,
+// code is added when more script files are loaded and you can load more file any time
---
+++ js assingment_2_3_4_/assgn.js
@@ -0,0 +1,153 @@
+//ass2 task1
+// let num1 = prompt('Enter the number ');//10
+// // num1 =parseInt (prompt ('Enter the number'))
+
+// let num2 = prompt('enter another number');//20
+
+// console.log( "The sum is :" + ' ' + num1 + num2);//1020
+// console.log( 'The diffrence is: ' + " " +num1 - num2);//-10
+// console.log( num1 * num2);//200
+// console.log( num1 / num2);//0.5 
+
+//task 2
+
+// let fruits;
+// let vegetables;
+// let schools;
+// let college;
+// let vechiles;
+// console.log(fruits);
+// console.log(vegetables);
+// console.log(schools);
+// console.log(college);
+// console.log(vechiles);
+
+// var names;
+// var subject;
+// var int = 1;
+// var init =2;
+// var num;
+// console.log(names);
+// console.log(subject);
+// console.log(int);
+// console.log(init);
+// console.log(num);
+
+//task3
+// const a = 5;
+// console.log(a);
+
+// let b =10;
+// console.log(b);
+// b = a;
+// console.log(b);
+
+// task4
+/*let flimName= 'Titanic', actorName='Leorando ';
+console.log( flimName +' ' +actorName);*/
+
+//assingment 3
+
+// let a=prompt('enter intergers');
+// console.log(a);
+/*num = parseFloat(prompt("Enter integers you want"));
+console.log(num);*/
+
+// let a=parseInt(prompt("Enter number"));
+// a=a+1;
+// console.log(a);
+
+// var num="999";
+// num = parseInt(num);
+// num=num-1;
+// console.log(num);
+
+// task4
+// var myFreind = 'Sumina , Awa';
+// console.log(myFreind);
+// console.log(typeof myFreind);
+
+//task5
+// let string1 = 'Ram Said,\" Programming is a fun game."';
+// console.log(string1);
+
+//task5
+// let email={
+//     sentTo  :'abc1234@gmail.com',
+//     sentFrom :" tome00@gmail.com",
+//     sent: function()
+//     {
+//         console.log(' message sent');
+//     }
+// };
+
+// email.sent();
+// console.log(email.sentTo);
+
+//EXTRAS
+// Task 1
+// var myAdress=
+// {
+//      myDistrict : 'Bhaktapur',
+//      myCity : 'Sallaghari',
+// };
+
+// console.log(myAdress.myCity);
+
+// TAsk2
+// var isProgrammingEngaging = true ;
+
+// TASK 3
+// var myAge = 20;
+
+// Task 4
+// var isTotalUsers = null;
+// console.log(typeof isTotalUsers);//object
+// console.log( isTotalUsers);//null
+
+// Task5
+// var myPhoneNumber = 9801001010;
+// console.log(myPhoneNumber);
+
+//Task 6
+// let myLaptopDetails =
+// {
+//     companyName : "Dell",
+//     operatingSystem:'Windows 10',
+//     ram : '4GB',
+// };
+
+// task7
+// let studentInfo =
+// {
+//     subject1 : "Math",
+//     subject2 : "Science" ,
+//     totalMarks: 100,
+//     isStudent :true,
+//     studentName : function()
+//     {
+//         console.log("My name is Hiralal");
+//     }
+// };
+
+// console.log(typeof studentInfo.subject1);
+// console.log(typeof studentInfo.subject2);
+// console.log(typeof studentInfo. totalMarks);
+// console.log(typeof studentInfo.isStudent);
+// console.log(typeof studentInfo.studentName);
+// console.log(studentInfo);
+// console.log(typeof studentInfo);
+
+// Task8
+// var  a = prompt( 'Enter something');
+// console.log(typeof a);
+// console.log(a);
+
+//Task 
+// const BG_Color = 'red';
+
+//task10
+// const abc = 500;
+// console.log(abc);
+//  const abc = 500;
+//  console.log(abc);
